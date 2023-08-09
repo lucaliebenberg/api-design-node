0b92d5ddab1c0e4517bf72b02387d0d29e8b0b3d
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-08-09 16:40:46 +0200

    added routes

---
+++ src/router.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * Product
+ */
+router.get("/product", () => {});
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.post("/product/:id", () => {});
+router.delete("/product", () => {});
+
+/**
+ * Update
+ */
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.post("/update/:id", () => {});
+router.delete("/update", () => {});
+
+/**
+ * Update point
+ */
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.put("/updatepoint/:id", () => {});
+router.post("/updatepoint/:id", () => {});
+router.delete("/updatepoint", () => {});
