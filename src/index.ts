b78505f75e619d7c1f0c44bf9bdae9a474e5e169
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-08-16 09:49:46 +0200

    updated changes for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 // const app = require("./server");
 import * as dotenv from "dotenv";
 dotenv.config();
-
+import config from "./config";
 import app from "./server";
 
-app.listen(3001, () => {
-  console.log("====================================");
-  console.log("Server listening on http://localhost:3001");
-  console.log("====================================");
+app.listen(config.port, () => {
+  console.log(`Hello on http://localhost:${config.port}`);
 });
